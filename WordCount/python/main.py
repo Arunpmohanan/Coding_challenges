98a1e21b994e17360eb94b7fd8e2471b3eb3580a
Arunpmohanan <49001386+Arunpmohanan@users.noreply.github.com>
2026-05-12 08:55:40 +0000

    oops approach

--- WordCount/python/main.py
+++ WordCount/python/main.py
@@ -1,16 +1,28 @@
 class counter():
-     
+
+    def __init__(self, input_string, option):
+        self.input_string = input_string
+        self.option = option
+
+    def count(self):
+        self.word_count = 1 
+        self.input_string = self.input_string.strip(" ")  
+        print (f'in count functio***{self.input_string}***')  
+        for character in self.input_string:        
+            print("character",character)
+            if character == " ":
+                self.word_count =  self.word_count + 1
+                print("in space loop")
+            print ("word_count",self.word_count )
+        return self.word_count
 
 def main():
-    character_count = 1
-    string_input = "Hello from"
-    for character in string_input:
-        string_input.strip()
-        print("character",character)
-        if character == " ":
-           character_count =  character_count + 1
-    print ("character_count",character_count )         
-      
+
+     
+    string_input = "Hello from "
+
+    counter_input = counter(string_input,"")
+    print(counter_input.count())
+
 if __name__ == "__main__":
     main()
-
